4e9e203556351158dd8fc47ca60b32df8e7825fb
Antonio M. Amaya <amac@peregrin>
2015-03-17 12:51:14 +0100

    Trusted hosted should work now

--- build/utils-node.js
+++ build/utils-node.js
@@ -388,6 +388,9 @@ module.exports = {
       webapp.pckManifest = this.readZipManifest(appDir);
       webapp.metaData = this.getJSON(metaData);
       webapp.appStatus = utils.getAppStatus(webapp.metaData.type || 'web');
+      if (webapp.manifest.type === 'trusted') {
+        webapp.kind = "trusted";
+      }
     } else {
       webapp.appStatus = utils.getAppStatus(webapp.manifest.type);
     }
